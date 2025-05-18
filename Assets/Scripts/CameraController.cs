c59207c093506b4f8a603bb561daf10a70672e3f
Austin Szema <aszema16@gmail.com>
2025-05-18 01:36:05 -0400

    assets and stuff

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 vel;
+    public Transform targetLookat;
+
+    public void SetNewTarget(Transform target)
+    {
+        targetLookat = target;
+    }
+
+    public float distance = 10f;
+    private Vector2 offset;
+
+    private float initialDistance = 10f;
+
+    private void Start()
+    {
+        initialDistance = distance;
+    }
+
+    public void Simulate()
+    {
+        transform.rotation = Quaternion.AngleAxis(offset.x, Vector3.up) * Quaternion.AngleAxis(offset.y, Vector3.right);
+        transform.position = Vector3.SmoothDamp(transform.position,
+            targetLookat.position - transform.forward * distance, ref vel, Time.deltaTime);
+        transform.rotation =
+            Quaternion.LookRotation((targetLookat.position - transform.position).normalized, Vector3.up);
+        
+    }
+
+    private void OnValidate()
+    {
+        Simulate();
+    }
+
+    private void Update()
+    {
+        offset += new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y")) * 2f;
+        distance -= Input.GetAxis("Mouse ScrollWheel") * 5f;
+        distance = Mathf.Clamp(distance, 2f, initialDistance);
+        distance = Mathf.Max(distance, 1f);
+    }
+    public void LateUpdate()
+    {
+        Simulate();
+    }
+    
+    public void FixedUpdate()
+    {
+        Simulate();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,32 +10,8 @@ public class PlayerController : MonoBehaviour
     
     [Header("Ground Movement")] public float moveSpeed = 100f;
     public float groundDrag = 5f;
-
-    [Header("Air Movement")] public float jumpForce = 30f;
-    public float jumpCooldown = 0.25f;
-    public float airMultiplier = 1.5f;
-    [Space] public float groundPoundForce = -240f;
-    public float groundPoundCooldown = 1f;
-
-    [Space]
-    //public float gravityMultiplier = 2f;
-    [Space]
-    public float fallGravityMultiplier = 5f;
-
-    public float lowJumpGravityMultiplier = 3f;
-    public float baseGravity = -19.62f;
-
-    [Header("Keybinds")] public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode groundPoundKey = KeyCode.LeftShift;
-
-
-    private bool readyToJump = true;
-    private bool readyToGroundPound = true;
-    private bool canGroundPound = true;
-
-    [Header("Ground Check")] public float playerHeight;
+    
     public LayerMask whatIsGround;
-    private bool grounded;
 
     [Header("Camera")]
     private Transform cameraTransform;
@@ -46,10 +22,6 @@ public class PlayerController : MonoBehaviour
     Vector3 moveDirection;
 
     [SerializeField] Rigidbody rb;
-
-    [SerializeField] private UnityEvent onGroundPound;
-
-    private bool pressedGroundPoundKey = false;
     
 
     private void Start()
@@ -63,48 +35,17 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (pressedGroundPoundKey)
-        {
-            onGroundPound.Invoke();
-            StartCoroutine(ResetGroundPoundTimer());
-        }
-    }
-
-    private IEnumerator ResetGroundPoundTimer()
-    {
-        yield return new WaitForSeconds(0.2f);
-        
-        pressedGroundPoundKey = false;
-    }
-
     private void Update()
     {
-        //Debug.Log("Velocity: " + rb.velocity.magnitude);
-        // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.5f, whatIsGround);
-
-        Debug.Log("Player is grounded: " + grounded);
         MyInput();
         SpeedControl();
 
-        // handle drag
-        if (grounded)
-        {
-            rb.linearDamping = groundDrag;
+        rb.linearDamping = groundDrag;
             
-            if (verticalInput < 0.1f && horizontalInput < 0.1f)
-            {
-                rb.linearDamping = 10;
-            }
-        }
-        else
+        if (verticalInput < 0.1f && horizontalInput < 0.1f)
         {
-            rb.linearDamping = 0;
+            rb.linearDamping = 10;
         }
-        
-
         SetPlayerRotation();
     }
 
@@ -121,59 +62,12 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
-        AddGravity();
         
     }
-
-    private void AddGravity()
-    {
-        // Custom gravity control
-        if (rb.linearVelocity.y < 0) // Falling
-        {
-            rb.AddForce(baseGravity * fallGravityMultiplier * Vector3.up,
-                ForceMode.Acceleration);
-        }
-        /*else if (rb.linearVelocity.y > 0) // Letting go of jump
-        {
-            rb.AddForce(baseGravity * lowJumpGravityMultiplier * Vector3.up,
-                ForceMode.Acceleration);
-        }*/
-        else // Rising normally
-        {
-            rb.AddForce(Vector3.up * baseGravity, ForceMode.Acceleration);
-        }
-    }
-
-
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-
-
-        // when to jump
-        if (Input.GetKeyDown(jumpKey) && readyToJump && grounded)
-        {
-            readyToJump = false;
-
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-
-        if (Input.GetKeyUp(groundPoundKey))
-        {
-            canGroundPound = true;
-        }
-
-        // when to ground pound
-        if (Input.GetKeyDown(groundPoundKey) && canGroundPound && readyToGroundPound && !grounded)
-        {
-            pressedGroundPoundKey = true;
-            canGroundPound = false;
-            GroundPound();
-            Invoke(nameof(ResetGroundPound), groundPoundCooldown);
-        }
     }
 
 
@@ -191,15 +85,8 @@ public class PlayerController : MonoBehaviour
 
         moveDirection = camForward * verticalInput + camRight * horizontalInput;
 
+        rb.AddForce(moveSpeed * 10f * moveDirection.normalized, ForceMode.Force);
         
-        // on ground
-        if (grounded)
-            rb.AddForce(moveSpeed * 10f * moveDirection.normalized, ForceMode.Force);
-
-        // in air
-        else if (!grounded)
-            rb.AddForce(airMultiplier * moveSpeed * 10f * moveDirection.normalized,
-                ForceMode.Force);
     }
 
 
@@ -214,44 +101,6 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
     }
-
-    private void Jump()
-    {
-        // reset y velocity
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-
-        if (pressedGroundPoundKey && Input.GetKey(groundPoundKey))
-        {
-            rb.AddForce(jumpForce * 1.25f * transform.up, ForceMode.Impulse);
-            
-        }
-        else
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-            
-        }
-    }
-
-    private void GroundPound()
-    {
-        // reset y velocity
-        //rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        rb.linearVelocity = Vector3.zero; 
-            
-        rb.AddForce(transform.up * groundPoundForce, ForceMode.Impulse);
-    }
-
-    private void ResetJump()
-    {
-        readyToJump = true;
-    }
-
-    private void ResetGroundPound()
-    {
-        readyToGroundPound = true;
-        canGroundPound = true;
-    }
+    
     
 }
